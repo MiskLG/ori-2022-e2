a056366c99440855e5ed27bd6bcc9e46ebd6046b
MiskLG <hornjak.ra139.2019@uns.ac.rs>
2022-05-28 22:02:20 +0200

    Finished 04-1-linreg

--- 04-1-linreg/src/iq.py
+++ 04-1-linreg/src/iq.py
@@ -1,3 +1,19 @@
-# TODO 4: implementirati primenu visestruke linearne regresije
-# nad podacima iz datoteke "data/iq.tsv".
-# Korisiti implementaciju linearne regresije u alatu scikit-learn
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+if __name__ == '__main__':
+    data_x = []
+    data_y = []
+
+    with open('../data/iq.tsv', 'r') as file:
+        for line in file:
+            try:
+                row = [float(num) for num in line.split()]
+                data_x.append(row[1:])
+                data_y.append(row[0])
+            except:
+                pass
+    lr = LinearRegression().fit(data_x, data_y)
+    print(lr.coef_)
+    print(lr.intercept_)
--- 04-1-linreg/src/linreg_simple.py
+++ 04-1-linreg/src/linreg_simple.py
@@ -3,17 +3,29 @@ import matplotlib.pyplot as plt
 
 
 def linear_regression(x, y):
-    slope = 0.0  # nagib linije
-    intercept = 0.0  # tacka preseka na y-osi
-    # TODO 1: implementirati linearnu regresiju
+    n = len(x)
+    slope_upper_sum = 0.0
+    slope_lower_sum = 0.0
+    x_avg = sum(x) / float(n)
+    y_avg = sum(y) / float(n)
+    # Calculating slope
+    for xi, yi in zip(x, y):
+        slope_upper_sum += (xi - x_avg) * (yi - y_avg)
+        slope_lower_sum += (xi - x_avg)**2
+
+    slope = slope_upper_sum / slope_lower_sum
+    # Calculating intercept
+    intercept_sum = 0.0
+    for xi, yi in zip(x, y):
+        intercept_sum += yi - slope * xi
+
+    intercept = intercept_sum / n  # tacka preseka na y-osi
 
     return slope, intercept
 
 
 def predict(x, slope, intercept):
-    # TODO 2: implementirati racunanje y na osnovu x i parametara linije
-
-    return 0.0
+    return intercept + x * slope
 
 
 def create_line(x, slope, intercept):
